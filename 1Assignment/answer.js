4ab2ab2cff010ec02244565c11034d054c238db7
kawsarNK <mdkawsarnk78@gmail.com>
2026-09-03 20:17:43 +0600

    solve problem 2

--- 1Assignment/answer.js
+++ 1Assignment/answer.js
@@ -8,3 +8,25 @@ function describeValue(value) {
     return `${type} | falsy`;
   }
 }
+
+
+// Question No. 2
+function getDayType(day) {
+  const lowerDay = day.toLowerCase();
+
+  switch (lowerDay) {
+    case "friday":
+    case "saturday":
+      return "Weekend";
+
+    case "sunday":
+    case "monday":
+    case "tuesday":
+    case "wednesday":
+    case "thursday":
+      return "Working Day";
+
+    default:
+      return "Invalid Day";
+  }
+}
